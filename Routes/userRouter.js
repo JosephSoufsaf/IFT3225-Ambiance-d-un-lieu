dc7bd7c89c42ed84cbf9903c723e312db3b817c0
Joseph <joseph.soufsaf@umontreal.ca>
2026-07-12 13:30:06 -0400

    added more content to the userRouter

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -7,21 +7,29 @@ const bcrypt = require('bcryptjs');
 
 
 router.post('/register', async (req, res) =>{
-  const password = req.body.password;
-  const email = req.body.email;
-  const username = req.body.username;
-  const hashedPassword = await bcrypt.hash(password, 10);
+  try{
+    const password = req.body.password;
+    const email = req.body.email;
+    const username = req.body.username;
 
-  if (!username || !email || !password){
-    return res.status(400).json({ success: false, error: "Champ requis manquant" });
+    if (!username || !email || !password){
+      return res.status(400).json({ success: false, error: "Champ requis manquant" });
+    }
+    
+    const emailTaken = await User.exists({email});
+    const usernameTaken = await User.exists({username});
+
+    if (emailTaken || usernameTaken){
+      return res.status(409).json({success: false, errror: "Email ou nom d'utilisateur deja utulise"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({hashedPassword});
+    return res.status(201).json({success:true, data: newUser});
+
+  }catch(error){
+    return res.status(400).json({success:false, error: error.message});
+    
   }
-  
-  const emailTaken = await User.exists({email});
-  const usernameTaken = await User.exists({username});
-
-  if (emailTaken || usernameTaken){
-    return res.status(409).json({success: false, errror: "Email ou nom d'utilisateur deja utulise"})
-  }
-
 
 })
