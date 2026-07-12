2c169a1334e404bbb7b519eeb7cc9bf26b05e88e
Joseph <joseph.soufsaf@umontreal.ca>
2026-07-11 22:27:46 -0400

    Started userRoutes

---
+++ Routes/userRouter.js
@@ -0,0 +1,24 @@
+const User = require("../models/User");
+const express = require('express');
+const router = new expres.Router();
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+
+
+
+router.post('/register', async (req, res) =>{
+  const password = req.body.password;
+  const email = req.body.email;
+  const username = req.body.username;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  if (!username || !email || !password){
+    return res.status(400).json({ success: false, error: "Champ requis manquant" });
+  }
+
+  
+
+
+
+
+})
